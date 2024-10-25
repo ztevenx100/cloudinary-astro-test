a6b6c350c5a9d2f31c5c1d0924e468d6850d1b12
Pedro Steven Castiblanco <pcastiblanco@megasoft.com.co>
2024-10-24 23:55:40 -0500

    TASK: add images collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
+    experimental:{
+        contentLayer: true,
+    }
 });
